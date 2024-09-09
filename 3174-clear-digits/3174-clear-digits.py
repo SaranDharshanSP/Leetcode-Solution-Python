473d7c15912807a1d8736cbacf60a0760adb914a
Saran Dharshan S.P <sarandharshanpushparaj@gmail.com>
2024-09-09 22:39:45 +0530

    Time: 32 ms (85.36%), Space: 16.6 MB (23.92%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        stk = []
+        for i in s:
+            if i.isalpha() :stk.append(i)
+            else: stk.pop()
+        return ''.join(stk)
